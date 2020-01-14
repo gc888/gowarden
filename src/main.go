bee3705575cea255feeca4813e41e32f288b3c0a
liubo <liubolovelife@gmail.com>
2020-01-15 00:01:27 +0800

    translate

--- src/main.go
+++ src/main.go
@@ -1,11 +1,10 @@
 package main
 
-import "flag"
+import (
 
-import "fmt"
+)
+
 
 func main() {
-	var ip = flag.Int("flagname", 1234, "help message for flagname")
-	flag.Parse()
-	fmt.Println("ip has value ", *ip)
+    
 }
